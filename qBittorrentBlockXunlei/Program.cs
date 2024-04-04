de5d013792d9e9d8538fc7850020255714fa46e4
Tony Hsieh <tonyhsie@hotmail.com>
2024-04-05 01:51:54 +0800

    v240405
    1. Ban the client "Taipei-Torrent dev*" (https://github.com/anacrolix/torrent/discussions/891#discussioncomment-8331808) & "trafficConsume" (https://github.com/anacrolix/torrent/discussions/891#discussioncomment-8453792)

--- qBittorrentBlockXunlei/Program.cs
+++ qBittorrentBlockXunlei/Program.cs
@@ -38,7 +38,7 @@ namespace qBittorrentBlockXunlei
         static readonly string sProgressFieldText = "\"progress\":";
         static readonly string sUploadedFieldText = "\"uploaded\":";
 
-        static readonly List<string> lsBannedClients = new List<string>() { "-XL", "Xunlei", "7.", "aria2", "Xfplay", "dandanplay", "FDM", "go.torrent", "Mozilla", "github.com/anacrolix/torrent (devel) (anacrolix/torrent unknown)", "dt/torrent/" };
+        static readonly List<string> lsBannedClients = new List<string>() { "-XL", "Xunlei", "7.", "aria2", "Xfplay", "dandanplay", "FDM", "go.torrent", "Mozilla", "github.com/anacrolix/torrent (devel) (anacrolix/torrent unknown)", "dt/torrent/", "Taipei-Torrent dev", "trafficConsume" };
 
         static void CCEHandler(object sender, ConsoleCancelEventArgs args)
         {
@@ -50,7 +50,7 @@ namespace qBittorrentBlockXunlei
 
         static async Task Main(string[] args)
         {
-            Console.Title = "qBittorrentBlockXunlei v240330";
+            Console.Title = "qBittorrentBlockXunlei v240405";
 
             Console.OutputEncoding = Encoding.UTF8;
             Console.CancelKeyPress += new ConsoleCancelEventHandler(CCEHandler);
